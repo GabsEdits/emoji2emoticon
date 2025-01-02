b255c9d5d6634174d3dd7e6b46dea599a63d4d8d
Gabriel Cozma <me@gxbs.dev>
2025-01-02 23:00:29 +0200

    feat(fe): move links to the top, and icons

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,46 +3,47 @@ import CovertionElement from "../islands/CovertionElement.tsx";
 export default function Home() {
   return (
     <main class="mt-20 h-[90vh]">
-      <h1 class="font-black mb-1 text-3xl relative">
-        Emoji to Emoticon
-      </h1>
-      <small class="mb-10 block">
-        Convert emojis to emoticons and <b>vice versa</b>
-      </small>
+      <div class="absolute top-5 right-5 flex flex-row gap-2 items-center justify-center">
+        <a
+          href="https://github.com/GabsEdits/emoji2emoticon"
+          target="_blank"
+          class="text-black dark:text-zinc-300 bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 hover:dark:bg-zinc-700 hover:text-black dark:hover:text-white px-3 py-2 rounded-xl transition-colors"
+        >
+          <i class="fab fa-github text-xl"></i>
+        </a>
+        <a
+          href="https://e2e-api.deno.dev"
+          target="_blank"
+          class="text-black dark:text-zinc-300 bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 hover:dark:bg-zinc-700 hover:text-black dark:hover:text-white px-3 py-2 rounded-xl transition-colors"
+        >
+          <i class="fas fa-server text-xl"></i>
+        </a>
+      </div>
+      <div class="flex flex-col gap-2 justify-center items-center">
+        <h1 class="font-black text-3xl">
+          Emoji to Emoticon
+        </h1>
+        <small class="mb-10">
+          Convert emojis to emoticons and <b>vice versa</b>
+        </small>
+      </div>
 
       <CovertionElement />
 
       <footer class="flex flex-col gap-3 mt-10 items-center justify-center">
-        <div class="flex flex-row gap-2">
-          <a
-            href="https://github.com/GabsEdits/emoji2emoticon"
-            target="_blank"
-            class="underline text-zinc-600 dark:text-zinc-300 dark:hover:text-zinc-200 hover:text-zinc-950 transition-colors font-extrabold duration-500"
-          >
-            Source Code
-          </a>
-
-          <a
-            href="https://e2e-api.deno.dev"
-            target="_blank"
-            class="underline text-zinc-600 dark:text-zinc-300 dark:hover:text-zinc-200 hover:text-zinc-950 transition-colors font-extrabold duration-500"
-          >
-            API
-          </a>
-        </div>
-
         <p>
           Made with {Math.random() < 0.5 ? "❤️" : "<3"} for{" "}
           <a
-            class="font-black underline text-black dark:text-white transition-colors"
+            class="font-black underline text-black dark:text-white transition-opacity hover:opacity-50 hover:text-black dark:hover:text-white"
             href="https://highseas.hackclub.com"
           >
             Highseas
           </a>
         </p>
         <p class="flex flex-col gap-1 sm:hidden lg:block">
-          <small>
-            <b>Pro Tip:</b> Press Space to copy the result
+          <small class="opacity-50">
+            <i class="fas fa-lightbulb"></i> <b>Pro-Tip:</b>{" "}
+            Press Space to copy the result
           </small>
         </p>
       </footer>
